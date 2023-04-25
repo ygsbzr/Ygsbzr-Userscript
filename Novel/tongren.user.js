8b15b6af05205d0053301ced348dec62b83bb3fc
YG <1440412630@qq.com>
2023-04-25 12:28:38 +0800

    Update novel

--- Novel/tongren.user.js
+++ Novel/tongren.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Remove ADV
 // @namespace    Ygsbzr
-// @version      0.1
+// @version      1.0
 // @description  移除广告
 // @author       Ygsbzr
 // @match        https://trxs.cc/tongren/*/*.html
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        unsafeWindow
+// @run-at       document-end
 // @updateURL    https://raw.githubusercontent.com/ygsbzr/Ygsbzr-Userscript/main/Novel/tongren.user.js
 // @downloadURL  https://raw.githubusercontent.com/ygsbzr/Ygsbzr-Userscript/main/Novel/tongren.user.js
 // ==/UserScript==
@@ -16,7 +17,9 @@
     RemoveADS();
 })();
 function RemoveADS(){
-    var iframe=document.getElementsByTagName("iframe");
-    iframe[0].previousElementSibling.remove();
-
+    var advscipt=document.querySelector("body > script:nth-child(5)");
+    advscipt.parentElement.removeChild(advscipt);
+    var advdiv=document.querySelector("iframe");
+    advdiv.previousElementSibling
+    advdiv.parentElement.removeChild(advdiv);
 }
